59a61d78de325e128d3a2a311f144031301cdf33
Wesley DuSell <dusellfam@gmail.com>
2023-05-03 23:36:51 -0400

    Baseline Setup

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,13 +6,12 @@ import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'ngx-home',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule
+  ],
 })
-export class HomeComponent {
-
-  newProcess() {
-    console.log('newProcess');
-  }
-}
+export class HomeComponent { }
